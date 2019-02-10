cba07dc020a1eed74fb4e82c6e8f3cb5f598ddb3
Dixit Bhatta <uclid_dixit@yahoo.com>
2019-02-10 18:16:55 -0500

    Create CandidatePoint.java

---
+++ java/CandidatePoint.java
@@ -0,0 +1,15 @@
+public class CandidatePoint {
+	public int xlocation;
+	public int ylocation;
+	
+	/**
+	 * @author Dixit Bhatta
+	 * @param xlocation x-coordinate in the grid
+	 * @param ylocation y-coordinate in the grid
+	 */
+	public CandidatePoint(int xlocation, int ylocation) {
+		this.xlocation = xlocation;
+		this.ylocation = ylocation;
+	}
+
+}
